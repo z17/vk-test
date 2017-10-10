de741622ec919e41845eb6afc9aeb9db539ae168
Maksim Iliukovich <maksim_iliukovich@epam.com>
2017-10-10 14:27:30 +0300

    test data

---
+++ db/2017-10-10-1.sql
@@ -0,0 +1,49 @@
+INSERT INTO product (name, description, price, img) VALUES (
+  'Логотип',
+  'Новый, свежий логотип',
+  1500,
+  'https://pp.userapi.com/c837721/v837721567/5ce2a/oNTd2QBCi5Y.jpg'
+);
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'Рекламный пост на Лентаче',
+  '1600000 участников, охват каждой записи 180 тысяч. Работа пикчера в подарок.',
+  88500,
+  'https://pp.userapi.com/c636718/v636718506/68478/IF4VnReVZGA.jpg'
+);
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'Вернуть стену',
+  'Альтернатива - не заходить в вк весь день и тогда администрация вернёт стену',
+  999999999,
+  'https://pp.userapi.com/c774/g20894073/a_e496a6cc.jpg'
+);
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'VK for Android',
+  'Вы сможете отправлять сообщения, делиться историями и фотографиями, смотреть видео и прямые трансляции, слушать музыку, играть в игры, вступать в сообщества и открыть целый мир талантливых авторов. Но зачем?',
+  30000,
+  'https://lh3.googleusercontent.com/8OiHVtg9BUN9Ms3LjD7RBhWGimOBSUoo7hdPymXcl-nIXeqA3C6La7qK59squ-emgQ=w300-rw'
+);
+
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'Экскурсия в башню Зингера',
+  'Приглашаем на уникальную экскурсию в одно из самых запоминающихся зданий Санкт-Петербурга — Дом Книги.',
+  500,
+  'http://np-travel.spb.ru/assets/gallery/79/481.jpg'
+);
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'Толковый словарь русского языка',
+  'Если вдруг понадобится подсчитать количество слов в русском языке, словарь можно купить на 1м этаже Дома Книги',
+  368,
+  'http://www.spbdk.ru/upload/img/093/2670931.jpg'
+);
+
+INSERT INTO product (name, description, price, img) VALUES (
+  'Незабываемый вечер',
+  'Комментарии излишни',
+  4500,
+  'https://pp.userapi.com/c836625/v836625109/50208/6kJhNqKGBaQ.jpg'
+);
